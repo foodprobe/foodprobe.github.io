a099c3e2caf4360a19277e43390f18a607151c78
kzstaging <76408459+kzstaging@users.noreply.github.com>
2020-12-21 08:42:48 +0000

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,2 +1,2 @@
-var url = "http://178.62.92.239:1880/api/live-probe-reading";
-var authKey = "a3pfYWRtaW46S3NoRlB3Z1FpcEtsNzVKT3BBbG4="; //TODO - this is the same as the admin auth for the node-red ui
+var url = "http://integration.koolzone.com/api/live-probe-reading";
+var authKey = "a3pfYWRtaW46S3NoRlB3Z1FpcEtsNzVKT3BBbG4="; 
