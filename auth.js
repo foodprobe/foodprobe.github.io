45170299f70164bf7e141e503dccfc61d17ed2f1
kzstaging <76408459+kzstaging@users.noreply.github.com>
2020-12-21 08:50:41 +0000

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,2 +1,2 @@
 var url = "http://integration.koolzone.com/api/live-probe-reading";
-var authKey = "a3pfYWRtaW46S3NoRlB3Z1FpcEtsNzVKT3BBbG4="; 
+var authKey = "a29vbHpvbmVfYXBpOlY3U29pcU5YM3dGUDNCMlc5MXEyQW1QdU1aMU1yTXZi"; 
